bcc91869083f316d99e6dcdf7d791cbe4d32be44
MaxSouza88 <souza1988@hotmail.com>
2022-08-24 17:56:41 -0300

    Initial commit

---
+++ src/test/java/com/Desafio/GerConta/GerContaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desafio.GerConta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
