fea09d06e59ffa7e68caa82aa9f5dd35238fdb4b
MaxSouza88 <souza1988@hotmail.com>
2022-09-13 08:46:02 -0300

    Atualizando Projeto

---
+++ src/main/java/com/Desafio/GerConta/model/ContasReceberModel.java
@@ -0,0 +1,46 @@
+package com.Desafio.GerConta.model;
+
+import com.Desafio.GerConta.model.enums.StatusEnum;
+import com.Desafio.GerConta.model.enums.TipoEnum;
+import com.Desafio.GerConta.model.enums.TipoRecebimento;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contasReceber")
+public class ContasReceberModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "recebimento", length = 100, nullable = false)
+    private String recebimento;
+
+    @Column(name = "valor_recebido", length = 10, nullable = false)
+    private BigDecimal valorRecebido;
+
+    @Enumerated(value = EnumType.STRING)
+    private TipoRecebimento tipoRecebido;
+
+    @Column(name = "data_vencimento", nullable = false)
+    private LocalDate dataDeVencimento;
+
+    @Column(name = "data_recebimento")
+    private LocalDateTime dataDeRecebimento;
+
+    @Enumerated(value = EnumType.STRING)
+    private StatusEnum status;
+
+}
